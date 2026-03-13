506533f773c561d0904223d4a380f1915fea70f8
Daksh-V27 <dakshdev0007@gmail.com>
2026-03-13 10:44:29 +0530

    arrays in js

---
+++ array.js
@@ -0,0 +1,13 @@
+let collection=[
+    {},
+    [],
+    true,
+    "john",
+    function(){},
+    900909090,
+    undefined,
+    null,
+    new string("abc"),
+    new date(),
+];
+console.log(collection);
